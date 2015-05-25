6f7bece583343ce2c2d5f595b8e3bacf9bfe4ef8
Chendi <di.chen@innonetwork.com>
2015-05-25 17:14:27 +0800

    init project

---
+++ CategoryProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryProject
+//
+//  Created by Chendi on 15/5/25.
+//  Copyright (c) 2015年 Cindy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CategoryProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CategoryProject
+//
+//  Created by Chendi on 15/5/25.
+//  Copyright (c) 2015年 Cindy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
